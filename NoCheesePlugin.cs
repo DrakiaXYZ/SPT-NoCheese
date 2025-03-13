4641f2f38c1d1bd4c2c5a58075e100319091c224
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-03-12 22:31:30 -0700

    Initial commit

---
+++ NoCheesePlugin.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx;
+using Comfort.Common;
+using EFT;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Reflection;
+using HarmonyLib;
+using Newtonsoft.Json;
+using SPT.Common.Http;
+using SPT.Common.Utils;
+using EFT.Interactive;
+using EFT.InventoryLogic;
+
+namespace DrakiaXYZ.NoCheese
+{
+    [BepInPlugin("xyz.drakia.nocheese", "DrakiaXYZ-NoCheese", "1.0.0")]
+    [BepInDependency("com.SPT.core", "3.11.0")]
+    public class NoCheesePlugin : BaseUnityPlugin
+    {
+        private static FieldInfo _raidSettingsField;
+        private static FieldInfo _dateTimeField;
+
+        public void Awake()
+        {
+            _raidSettingsField = AccessTools.Field(typeof(LocalGame), "localRaidSettings_0");
+            _dateTimeField = AccessTools.Field(typeof(LocalGame), "dateTime_0");
+
+            Application.quitting += Quit;
+        }
+
+        private static void Quit()
+        {
+            // If we're not in a game, don't do anything
+            if (!Singleton<AbstractGame>.Instantiated)
+            {
+                return;
+            }
+
+            // If we can't convert the game to an abstract game, don't do anything
+            LocalGame game = Singleton<AbstractGame>.Instance as LocalGame;
+            if (game == null)
+            {
+                return;
+            }
+
+            // Kill the player
+            GamePlayerOwner.MyPlayer.OnDead(EDamageType.Existence);
+
+            // Note for future me:
+            // Look at the `BaseLocalGame` method that calls `LocalRaidEnded`, as well as `LocalRaidEnded` itself for
+            // what classes are used below
+
+            // Tell the server we went MIA
+            var profile = GamePlayerOwner.MyPlayer.Profile;
+            var raidSettings = _raidSettingsField.GetValue(game) as LocalRaidSettings;
+            var lostInsuredItems = game.method_12();
+            var transferItems = game.method_13();
+            var duration = EFTDateTimeClass.Now - (DateTime)_dateTimeField.GetValue(game);
+
+            GClass1959 results = new GClass1959
+            {
+                profile = (new GClass1998(profile, GClass2007.Instance)).ToUnparsedData(Array.Empty<JsonConverter>()),
+                result = ExitStatus.MissingInAction,
+                killerId = "",
+                killerAid = "",
+                exitName = "",
+                inSession = true,
+                favorite = (profile.Info.Side == EPlayerSide.Savage),
+                playTime = (int)duration.TotalSeconds,
+                ProfileId = profile.Id
+            };
+
+            var data = new Class77<string, GClass1959, GClass1319[], Dictionary<string, GClass1319[]>, GClass1961>(raidSettings.serverId, results, lostInsuredItems, transferItems, null);
+
+            RequestHandler.PostJson("/client/match/local/end", Json.Serialize(data));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DrakiaXYZ-NoCheese")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DrakiaXYZ-NoCheese")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38c330d5-9a24-4339-ab28-3cb8d29bb00a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
